a932195ad42b35e1c3d7c4a647b2a1b4e7df93bd
jmilad1 <jmilad@lululemon.com>
2025-12-04 11:17:51 -0800

    i dont even know how i backfilled

--- src/app/api/test/backfill/route.ts
+++ src/app/api/test/backfill/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-import { backfillStatementMerchantMap } from "@/app/utils/override";
+// import { backfillStatementMerchantMap } from "@/app/utils/override";
 import logger from "@/lib/logger";
 
 export async function GET() {
     try {
-        const result = await backfillStatementMerchantMap();
-        return NextResponse.json(result);
+        // const result = await backfillStatementMerchantMap();
+        return NextResponse.json({'hello': 'world'});
     } catch (error) {
         logger.error(`Error in backfill API route: ${JSON.stringify(error, null, 2)}`);
         return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
